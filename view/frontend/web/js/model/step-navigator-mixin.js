5401dc985371a67367561492866ef7438a6e5444
Dawid Udziela <udziela.dawid@gmail.com>
2020-05-12 17:45:09 +0200

    add licence info

--- view/frontend/web/js/model/step-navigator-mixin.js
+++ view/frontend/web/js/model/step-navigator-mixin.js
@@ -1,3 +1,10 @@
+/*
+ *  @category   Hatimeria
+ *  @author      (office@hatimeria.com)
+ *  @copyright  Copyright (c) 2020 Hatimeria Sp. z o.o. Sp. k. ( https://www.hatimeria.com/ )
+ *  @license    (https://www.gnu.org/licenses/gpl-3.0.html)
+ */
+
 define([
     'jquery',
     'mage/utils/wrapper',
